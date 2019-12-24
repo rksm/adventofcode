d3997f7562226ed8e4b7671b5a0d9535cc0234af
Robert Krahn <robert.krahn@gmail.com>
2019-12-24 10:18:54 +0100

    make clj run as main

--- 2019/clj/src/aoc_2019/main.clj
+++ 2019/clj/src/aoc_2019/main.clj
@@ -1,3 +1,8 @@
-(ns aoc-2019.main)
+(ns aoc-2019.main
+  (:require [aoc-2019.day02 :as day02]))
 
-(println "hello world")
+(defn -main []
+  (time
+   (println (day02/solve-1)))
+  (time
+   (println (day02/solve-2))))
